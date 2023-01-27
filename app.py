9d8c849eeb8499ae3977c4039c5904f9d942cefb
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-01-28 02:48:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello!</h1>"
+def hello():
+    return "Flask inside Docker!!"
+
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0',port=port)
